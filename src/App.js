b4d127d30c4f6a7bb07074c29efe5b52f75b91fc
dahalh <dahalh@outlook.com>
2022-04-07 16:35:09 +1000

    add and delete function added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,52 @@
+import { nanoid } from "nanoid";
+import { useState } from "react";
+import NotesList from "./components/NotesList";
+
 const App = () => {
-  return <div className="App"></div>;
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: "This is my first note!",
+      date: "12/12/2012",
+    },
+    {
+      id: nanoid(),
+      text: "This is my second note!",
+      date: "13/12/2012",
+    },
+    {
+      id: nanoid(),
+      text: "This is my third note!",
+      date: "14/12/2012",
+    },
+  ]);
+
+  const addNote = (text) => {
+    const date = new Date();
+    const newNote = {
+      id: nanoid(),
+      text: text,
+      date: date.toLocaleDateString(),
+    };
+
+    const newNotes = [...notes, newNote];
+    setNotes(newNotes);
+  };
+
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+  };
+
+  return (
+    <div className="container">
+      <NotesList
+        notes={notes}
+        handleAddNote={addNote}
+        handleDeleteNote={deleteNote}
+      />
+    </div>
+  );
 };
 
 export default App;
